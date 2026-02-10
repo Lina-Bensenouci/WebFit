833c317283c616dcdc4a934b7f332d8fad1418c0
Lina-Bensenouci <119866419+Lina-Bensenouci@users.noreply.github.com>
2026-02-09 21:14:18 -0500

    Entete mobile et desktop terminé avec animation

--- FitWeb/src/composants/Entete.jsx
+++ FitWeb/src/composants/Entete.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
 import "./Entete.scss";
 import LogoWebFit from "../Images/Logo4WebFit.png";
 import Login from "../Images/Login2.png";
@@ -18,60 +18,34 @@ export default function Entete() {
 
         {/* Nav desktop */}
         <nav className="menu-desktop-nav">
-          {/* Nouvelle page */}
-          <Link to="/">Accueil</Link>
-
-          {/* Nouvelle page */}
-          <Link to="/abonnements">Abonnements</Link>
-
-          {/* Nouvelle page */}
-          <Link to="/cours">Cours en groupe</Link>
-
-          {/* Section */}
+          <NavLink to="/" className={({ isActive }) => isActive ? "active" : ""}>Accueil</NavLink>
+          <NavLink to="/abonnements" className={({ isActive }) => isActive ? "active" : ""}>Abonnements</NavLink>
+          <NavLink to="/cours" className={({ isActive }) => isActive ? "active" : ""}>Cours en groupe</NavLink>
           <a href="#about">À propos</a>
-
-          {/* Section */}
           <a href="#hours">Horaires</a>
         </nav>
 
         {/* Login + Burger mobile */}
         <div className="entete-droite">
-          {/* Login */}
           <img src={Login} alt="Login" className="login-icon" />
-
-          {/* Burger mobile */}
           <button className="burger" onClick={() => setIsOpen(true)}>☰</button>
         </div>
       </header>
 
       {/* Menu burger mobile */}
       <div className={`menu ${isOpen ? "open" : ""}`}>
-        {/* Bouton fermer */}
         <button className="close" onClick={() => setIsOpen(false)}>✕</button>
 
-        {/* Logo dans le menu */}
         <div className="menu-logo">
           <Link to="/"><img src={LogoWebFit} alt="WebFit" /></Link>
         </div>
 
-        {/* Liens menu mobile */}
         <nav>
-          {/* Nouvelle page */}
-          <Link to="/" onClick={() => setIsOpen(false)}>Accueil</Link>
-
-          {/* Nouvelle page */}
-          <Link to="/abonnements" onClick={() => setIsOpen(false)}>Abonnements</Link>
-
-          {/* Nouvelle page */}
-          <Link to="/cours" onClick={() => setIsOpen(false)}>Cours en groupe</Link>
-          
-          {/* Section */}
+          <NavLink to="/" onClick={() => setIsOpen(false)} className={({ isActive }) => isActive ? "active" : ""}>Accueil</NavLink>
+          <NavLink to="/abonnements" onClick={() => setIsOpen(false)} className={({ isActive }) => isActive ? "active" : ""}>Abonnements</NavLink>
+          <NavLink to="/cours" onClick={() => setIsOpen(false)} className={({ isActive }) => isActive ? "active" : ""}>Cours en groupe</NavLink>
           <a href="#about" onClick={() => setIsOpen(false)}>À propos</a>
-
-          {/* Section */}
           <a href="#hours" onClick={() => setIsOpen(false)}>Horaires</a>
-
-          
         </nav>
       </div>
     </>
