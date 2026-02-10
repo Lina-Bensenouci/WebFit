7117fc99bc50e3f753c3a515bdba10cb65c1c5d9
Lina-Bensenouci <119866419+Lina-Bensenouci@users.noreply.github.com>
2026-02-09 20:42:55 -0500

    Entete mobile et desktop terminé pour l'affichage, animation à faire

--- FitWeb/src/composants/Entete.jsx
+++ FitWeb/src/composants/Entete.jsx
@@ -11,35 +11,66 @@ export default function Entete() {
     <>
       {/* Header */}
       <header className="entete">
+        {/* Logo cliquable vers accueil */}
         <div className="entete-logo">
-            <Link to="/"><img src={LogoWebFit} alt="WebFit" /></Link>
+          <Link to="/"><img src={LogoWebFit} alt="WebFit" /></Link>
         </div>
 
-        {/* Burger + Login collés */}
+        {/* Nav desktop */}
+        <nav className="menu-desktop-nav">
+          {/* Nouvelle page */}
+          <Link to="/">Accueil</Link>
+
+          {/* Nouvelle page */}
+          <Link to="/abonnements">Abonnements</Link>
+
+          {/* Nouvelle page */}
+          <Link to="/cours">Cours en groupe</Link>
+
+          {/* Section */}
+          <a href="#about">À propos</a>
+
+          {/* Section */}
+          <a href="#hours">Horaires</a>
+        </nav>
+
+        {/* Login + Burger mobile */}
         <div className="entete-droite">
+          {/* Login */}
           <img src={Login} alt="Login" className="login-icon" />
+
+          {/* Burger mobile */}
           <button className="burger" onClick={() => setIsOpen(true)}>☰</button>
         </div>
       </header>
 
-      {/* Menu burger */}
+      {/* Menu burger mobile */}
       <div className={`menu ${isOpen ? "open" : ""}`}>
+        {/* Bouton fermer */}
         <button className="close" onClick={() => setIsOpen(false)}>✕</button>
+
+        {/* Logo dans le menu */}
+        <div className="menu-logo">
+          <Link to="/"><img src={LogoWebFit} alt="WebFit" /></Link>
+        </div>
+
+        {/* Liens menu mobile */}
         <nav>
           {/* Nouvelle page */}
           <Link to="/" onClick={() => setIsOpen(false)}>Accueil</Link>
 
-          {/* Section */}
-          <a href="#about" onClick={() => setIsOpen(false)}>À propos</a>
-
           {/* Nouvelle page */}
           <Link to="/abonnements" onClick={() => setIsOpen(false)}>Abonnements</Link>
+
+          {/* Nouvelle page */}
+          <Link to="/cours" onClick={() => setIsOpen(false)}>Cours en groupe</Link>
+          
+          {/* Section */}
+          <a href="#about" onClick={() => setIsOpen(false)}>À propos</a>
 
           {/* Section */}
           <a href="#hours" onClick={() => setIsOpen(false)}>Horaires</a>
 
-          {/* Nouvelle page */}
-          <Link to="/cours" onClick={() => setIsOpen(false)}>Cours en groupe</Link>
           
         </nav>
       </div>
