450a8ee76c03307cb52a4353f3df803071a524c7
Lina-Bensenouci <119866419+Lina-Bensenouci@users.noreply.github.com>
2026-03-03 21:54:34 -0500

    ajout .env pour déploiement

--- FitWeb/vite.config.js
+++ FitWeb/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "", // Ajouter le nom du dépôt GitHub si on publie sur GH Pages : "/exemple/"
+  base: "/", // Ajouter le nom du dépôt GitHub si on publie sur GH Pages : "/exemple/"
   server: {
     headers: {
       // Cette ligne permet d'autoriser les popups et les messages cross-origin
